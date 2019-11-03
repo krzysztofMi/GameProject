8bab464511ad98ea87f09716a6b7932f0f415dd9
Krzosa <krzosa.karol@gmail.com>
2019-11-03 11:42:09 +0100

    Dodałem: podstawowy obiekt gracza, testowa scene
    Co zrobiłem:
    Dodałem obiekt gracza który potrafi poruszac sie do przodu do tylu na boki oraz przede wszystkim rozgladac.
    Z obiektu gracza zrobiłem prefab(https://docs.unity3d.com/Manual/Prefabs.html) tak samo z obiektu planszy.
    
    Jak zrobiłem:
    Obiektowi gracza dałem 2 kamery jedna tylko widzi bron, druga widzi otoczenie i nie widzi broni zrobiłem to na podstawie
    poczatku tego filmiku, nie jest za dobry jeżeli chodzi o zrozumienie skryptowania(https://www.youtube.com/watch?v=Sqb-Ue7wpsI&t=1856s)
    Obiekt gracza posiada tez prostokat ktory reprezentuje bron oraz cylinder reprezentujacy gracza, są one dziecmi glownego
    obiektu Player(rodzic) oznacza to, ze transformujac Player(przesuwanie, rotowanie) transformujemy tez jego dzieci
    w tym wypadku bron i cialo gracza plus kamery. Zaprogramowalem tez chodzenie(a raczej latanie) i rozgladanie
    opisane sa komentarzami i w dalszej sekcji.
    
    Skryptowanie:
    Jest zaskakująco intuicyjne jeżeli się ogarnie podstawy polecam link!!! https://docs.unity3d.com/Manual/CreatingAndUsingScripts.html
    bardzo dużo rozjaśnia do tego seria filmików yt unity o konkretnych zagadnieniach(2 min filmiki) https://www.youtube.com/watch?v=32JkMANaMpk&list=PLX2vGYjWbI0S9-X2Q021GUtolTqbUBB9B&index=12
    Do skryptowania póki co nie potrzebna byla mi znajomosc c# wystarczy C++ kojarzyc. Dokumentacja Unity jest calkiem ok.

---
+++ Assets/Prefabs/Player/FPSCameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCameraMovement : MonoBehaviour
+{
+    public float mousesSensitivity = 100.0f;
+    //tutaj deklarujemy zewnętrzny obiekt, gdy damy mu public to wyświetla się nowe pole w inspektorze
+    //w Unity w componencie który nazywa się tak jak skrypt
+    //i przeciągamy na to pole obiekt gracza ze sceny. Dzięki temu możemy na nim operować.
+    public GameObject player;
+
+    private float mouseX;
+    private float mouseY;
+
+    //TODO ograniczyc patrzenie w gore bo mozna fikolka zrobic
+    void Update()
+    {
+        //zwroccie uwage na to ze do zarzadzania kamera obracam nie tylko sama kamera, ale 
+        //takze graczem. Robie to dlatego, ze gdy obracalem kamera po X i Y to kamera obracala
+        //sie takze po Z jest to nie intuicyjne, ale poobracajcie sobie kostke po X i Y 
+        //w edytorze unity a zobaczycie, ze to faktyczne zachowanie 3D obiektow.
+
+        //deltaTime https://www.youtube.com/watch?v=Gcoj3llfzSw&list=PLX2vGYjWbI0S9-X2Q021GUtolTqbUBB9B&index=19
+        //GetAxis https://www.youtube.com/watch?v=MK4OmsViqMA&list=PLX2vGYjWbI0S9-X2Q021GUtolTqbUBB9B&index=16
+        //transform.Rotate https://www.youtube.com/watch?v=32JkMANaMpk&list=PLX2vGYjWbI0S9-X2Q021GUtolTqbUBB9B&index=12s
+        mouseX = Input.GetAxis("Mouse X")*mousesSensitivity*Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y")*mousesSensitivity*Time.deltaTime;
+        player.transform.Rotate(0, mouseX, 0); //operujemy na zewnętrznym obiekcie
+        transform.Rotate(-mouseY, 0, 0); //operujemy na obiekcie do którego jest ten skrypt przyczepiony
+        
+    }
+}
---
+++ Assets/Prefabs/Player/fpsPlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpsPlayerMovement : MonoBehaviour
+{
+    public float speed = 1.0f; //gdy damy public to pojawia się nowe pole w inspektorze tam gdzie
+    //skrypt jest przyczepiony. 1 Domyślna wartość
+    private float h, v;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //deltaTime https://www.youtube.com/watch?v=Gcoj3llfzSw&list=PLX2vGYjWbI0S9-X2Q021GUtolTqbUBB9B&index=19
+        //GetAxis https://www.youtube.com/watch?v=MK4OmsViqMA&list=PLX2vGYjWbI0S9-X2Q021GUtolTqbUBB9B&index=16
+        //transform.translate https://www.youtube.com/watch?v=32JkMANaMpk&list=PLX2vGYjWbI0S9-X2Q021GUtolTqbUBB9B&index=12
+        h = Input.GetAxis("Horizontal")*speed*Time.deltaTime;
+        v = Input.GetAxis("Vertical")*speed*Time.deltaTime;
+        transform.Translate(h, 0, v);
+    }
+}
