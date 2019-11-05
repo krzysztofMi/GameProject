e385dba4252e38bd39fbe3666c185e1d23b53242
Krzosa <krzosa.karol@gmail.com>
2019-11-05 17:02:56 +0100

    Add RTSCamera

---
+++ Assets/Prefabs/RTSCamera/RTSCameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSCameraMovement : MonoBehaviour
+{
+    public float speed=20.0f;
+    private float v, h;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        v = Input.GetAxis("Vertical")*Time.deltaTime*speed;
+        h = Input.GetAxis("Horizontal")*Time.deltaTime*speed;
+        transform.Translate(transform.forward * v);
+        transform.Translate(transform.right * h);
+    }
+}
---
+++ Assets/Prefabs/RTSCamera/cameraSwitcher.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSwitcher : MonoBehaviour
+{
+    private GameObject RTSCamera;
+    private GameObject player1; 
+    private GameObject fpsCamera1;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player1 = GameObject.Find("Player1");
+        fpsCamera1 = player1.transform.Find("PlayerCamera").gameObject;
+        RTSCamera = transform.Find("RTSCamera").gameObject;
+
+        fpsCamera1.SetActive(false);
+        RTSCamera.SetActive(true);
+        player1.GetComponent<fpsPlayerMovement>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("1"))
+        {
+            fpsCamera1.SetActive(true);
+            RTSCamera.SetActive(false);
+            player1.GetComponent<fpsPlayerMovement>().enabled = true;
+        }
+        if (Input.GetButton("`"))
+        {
+            fpsCamera1.SetActive(false);
+            RTSCamera.SetActive(true);
+            player1.GetComponent<fpsPlayerMovement>().enabled = false;
+        }
+        
+    }
+}
