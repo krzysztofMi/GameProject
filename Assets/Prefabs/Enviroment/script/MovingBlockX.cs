2485b82670cf4aff71f424eb4e6f26f10a021470
unknown <krzysztofmichalski987@gmail.com>
2019-11-08 16:06:05 +0100

    jumping challange

---
+++ Assets/Prefabs/Enviroment/script/MovingBlockX.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBlockX : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private Vector3 movement;
+    public bool forwardDirection;
+    private int times = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (forwardDirection)
+        {
+            movement.x = speed;
+        }
+        else
+        {
+            movement.x = -speed;
+        }
+        times++;
+        transform.Translate(movement * Time.deltaTime);
+        if (times == 100)
+        {
+            times = 0;
+            if (forwardDirection)
+            {
+                forwardDirection = false;
+            }
+            else
+            {
+                forwardDirection = true;
+            }
+        }
+    }
+}
